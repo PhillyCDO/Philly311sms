de1a641297d464d470bdc9f8264da1185e1804c4
Philadelphia Chief Data Officer <mark.headd@phila.gov>
2013-02-03 14:40:01 -0500

    Initial commit of files.

---
+++ Philly311sms/GetStatusModule.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Configuration;
+using System.Globalization;
+using Nancy;
+using Newtonsoft.Json.Linq;
+using Open311API;
+using Open311API.Exception;
+using Open311API.Structs;
+using Twilio;
+
+public class GetStatusModule : NancyModule
+{
+    // 311 API endpoint.
+    const string ENDPOINT = "http://www.publicstuff.com/api/open311/";
+
+    // Jursdiction identifier to use with API calls.
+    const string JURISDICTION_ID = "philadelphia-pa";
+
+    //Twilio credentials.
+    private string ACCOUNTSID = ConfigurationManager.AppSettings["TwilioAccountSID"].ToString();
+    private string AUTHTOKEN = ConfigurationManager.AppSettings["TwilioAuthToken"].ToString();
+    private string FROMNUMBER = ConfigurationManager.AppSettings["TwilioFromNumber"].ToString();
+
+    // Used for formatting response to SMS gateway.
+    private string response;
+    private TextInfo ti = new CultureInfo("en-US", false).TextInfo;
+
+    /// <summary>
+    /// Constructor.
+    /// </summary>
+    public GetStatusModule()
+    {
+        // Create new Open311 object instance.
+        var report = new Open311(ENDPOINT, JURISDICTION_ID);
+
+        // Create new Twilio object instance.
+        var twilio = new TwilioRestClient(ACCOUNTSID, AUTHTOKEN);
+
+        // Route for POST from SMS gateway.
+        Get["/getStatus"] = x =>
+        {
+            // Users callerID.
+            string userNumber = Request.Query["From"];
+
+            // Message sent with inbound SMS.
+            string serviceRequestID = Request.Query["Body"];
+
+            if (isNumber(serviceRequestID))
+            {
+                try
+                {
+
+                    JArray statusJson = JArray.Parse(report.GetServiceRequest(ResponseFormat.JSON, serviceRequestID));
+                    response = FormatResponse(statusJson);
+                }
+                catch (Open311Exception ex)
+                {
+                    response = "Sorry, I did not find that service request. :-(";
+                }
+            }
+            else
+            {
+                response = "Sorry, I could not look up service request ID " + serviceRequestID + ".";
+            }
+            twilio.SendSmsMessage(FROMNUMBER, userNumber, response);
+            return 200;
+        };
+    }
+
+    /// <summary>
+    /// Check to ensure that SMS body contains a valid numeric service ID.
+    /// </summary>
+    /// <param name="num">User suibmitted ID to be checked.</param>
+    /// <returns>bool</returns>
+    private bool isNumber(string num)
+    {
+        try
+        {
+            int id = Int32.Parse(num.Trim());
+            return true;
+        }
+        catch (Exception ex)
+        {
+            return false;
+        }        
+    }
+    
+    /// <summary>
+    /// Format SMS response.
+    /// </summary>
+    /// <param name="statusJson">JSON response from 311 API.</param>
+    /// <returns>Formatted response</returns>
+    private string FormatResponse(JArray statusJson)
+    {
+        response = "Service request: " + statusJson[0]["service_request_id"].ToString() + "\n";
+        response += "Type: " + statusJson[0]["service_name"].ToString() + "\n";
+        response += "Reported: " + statusJson[0]["requested_datetime"].ToString() + "\n";
+        response += "Status: " + ti.ToTitleCase(statusJson[0]["status"].ToString()) + "\n";
+        return response;
+    }
+}
---
+++ README.md
@@ -0,0 +1,25 @@
+#Philly311SMS
+
+This is a demo app that shows how to use the Philly311 API to enable text message lookups for the status of service requests.
+
+##Modules
+
+* [Open311](https://github.com/mheadd/csharp-open311)
+* [NancyFx](http://nancyfx.org/)
+* [JSON.NET](http://james.newtonking.com/projects/json-net.aspx)
+* [Twilio.NET](https://github.com/twilio/twilio-csharp)
+
+##Installation / Usage
+
+Clone this repo, then open the solution in Visual Studio.
+You'll need to install NancyFx, JSON.NET and Twilio.NET via the [NuGet package manager](http://nuget.org/):
+
+<pre>
+   PM> Install-Package Nancy.Hosting.Aspnet
+   PM> Install-Package Newtonsoft.Json
+   PM> Install-Package Twilio
+</pre>
+
+The C# class for Open311 is not yet availalbe via NuGet, so [clone the repo](https://github.com/mheadd/csharp-open311), open the solution, build, then move the resultant <code>.dll</code> file into your solution.
+
+You'll need a Twilio account. Add your account credentials in <code>Web.config</code> and your good to go. 
